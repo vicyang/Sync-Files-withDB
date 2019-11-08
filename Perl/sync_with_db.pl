ea741b52d7de403ef68bcf62637ca2366173fa5a
Vulkan <523066680@163.com>
2019-11-08 20:00:57 +0800

    Signed-off-by: Vulkan <523066680@163.com>

--- Perl/sync_with_db.pl
+++ Perl/sync_with_db.pl
@@ -17,37 +17,65 @@ my $src = encode('gbk', "E:\\Company\\S烧录程序_软件");
 my $spec = encode('gbk', "烧录");  # target folder name
 my $dst;
 my $drv;
-my $hash = {};
+my $src_tree = {};
+my $dst_tree = {};
+my $all_tree = {};
+my $src_files = [];
 
 $drv = find_target_drive( $spec );
 if ( defined $drv ) {
     $dst = "${drv}:\\${spec}";
 } else {
     printf "Target not found\n";
-    exit;
+    sleep 2; exit;
 }
 
 my $dbfile = "${drv}:\\tree.db";
 
 if (-e $dbfile) {
-   $hash = retrieve($dbfile);
+   $dst_tree = retrieve($dbfile);
 } else {
-    build_tree( $dst, $hash );
-    store($hash, $dbfile);
+    build_tree( $dst, $dst_tree );
+    store($dst_tree, $dbfile);
 }
 
-#print Dumper $hash;
-sync_files();
-update_db($hash, $dbfile);
+# 构建原目录文件哈希表
+build_tree( $src, $src_tree );
+#get_files_list( $src, $src_files );
+compare( $src_tree, $dst_tree, "" );
+exit;
+
+sync_files( $src_tree, $dst_tree );
+update_db( $dst_tree, $dbfile );
 printf "Done\n";
+
+sub compare
+{
+    my ($src_node, $dst_node, $path ) = @_;
+    my ($node);
+
+    #printf "%s\n", $path;
+    for my $k ( keys %$dst_node )
+    {
+        if (exists $src_node->{$k} )
+        {
+            compare( $src_node->{$k} , $dst_node->{$k}, "${path}\\${k}" );
+        }
+        else
+        {
+            printf "%s %s not exists\n", $path, $k;
+        }
+    }
+}
 
 sub sync_files
 {
+    my ( $src_tree, $dst_tree ) = @_;
     my $s = `dir /s /b $src`;
     my $src_path;
     my $hash_path;
-    open my $fh, "<:raw", \$s; 
-    while ($src_path = <$fh>)
+    open my $fh, "<:raw", \$s;
+    while ( $src_path = <$fh> )
     {
         $src_path =~s/\r?\n$//;
         $src_path =~/\Q${src}\E(.*)$/;
@@ -89,12 +117,19 @@ sub check
     return 1;
 }
 
+sub get_files_list
+{
+    my ($path, $ref) = @_;
+    @$ref = `dir /a /s /b $path`;
+    grep { s/\r?\n$// } @$ref;
+}
+
 # 遍历文件列表，构建哈希树
 sub build_tree
 {
     my ($wdir, $ref) = @_;
     print "Loading Directory Content ...\n";
-    my $s = `dir /s /b $wdir`;
+    my $s = `dir /a /s /b $wdir`;
     # string as file stream
     open my $fh, "<:raw", \$s; 
     while ($line = <$fh>)
